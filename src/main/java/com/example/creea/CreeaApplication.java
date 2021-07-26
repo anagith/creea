f8aeb0460fb14965628cc4462744dd744a8ef7a1
Anahiit <amartirosyan904@gmail.com>
2021-07-26 14:56:23 +0400

    creea

---
+++ src/main/java/com/example/creea/CreeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.creea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/creea/CreeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
