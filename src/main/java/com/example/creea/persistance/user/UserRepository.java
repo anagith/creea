6c780252db17d98d6c36204057bb6b7fef132858
Ligit2 <avoyan-lilit@mail.ru>
2021-07-29 11:47:21 +0400

    add animal service

--- src/main/java/com/example/creea/persistance/user/UserRepository.java
+++ src/main/java/com/example/creea/persistance/user/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.creea.persistance.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
 }
